2189cc9d9dd477cc5334092f7a9d7aab387d4619
John Brodnax <JBrodnax320@jbrodnax320.local>
2022-06-27 13:33:18 -0500

    added flask webserver test

---
+++ FlaskWSS/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_sock import Sock
+
+app = Flask(__name__)
+sock = Sock(app)
+
+
+@app.route('/')
+def index():
+    tmp = '{"hello":"world"}'
+    return tmp
+
+
+@sock.route('/echo')
+def echo(sock):
+    while True:
+        data = sock.receive()
+        sock.send(data)
