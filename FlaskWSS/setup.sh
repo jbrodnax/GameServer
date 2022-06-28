52bc57046eb7359ff0ab6d1f9a0a4108d91e6a9b
John Brodnax <JBrodnax320@jbrodnax320.local>
2022-06-28 10:36:09 -0500

    added install script and edited setup script

--- FlaskWSS/setup.sh
+++ FlaskWSS/setup.sh
@@ -2,10 +2,11 @@
 
 ## First copy app.py file to project dir.
 ## From project dir run:
-pip install python3.8-venv
-python3 -m venv venvtest
-source ./venvtest/bin/activate
+# pip install python3.8-venv
+# python3 -m venv venvtest
 
 ## From inside venv
-pip install flask flask-sock flask-sqlalchemy flask-login
-flask run --host=0.0.0.0
+echo "run:"
+echo 'export FLASK_APP=project'
+echo 'export FLASK_DEBUG=1'
+echo 'flask run --host=0.0.0.0'
