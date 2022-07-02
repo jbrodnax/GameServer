6eb5c26894fc717686bb2d663f52ed1d55f9a65d
John Brodnax <JBrodnax320@jbrodnax320.local>
2022-07-02 12:50:32 -0500

    why is it always 404

--- FlaskWS/app/main/routes.py
+++ FlaskWS/app/main/routes.py
@@ -1,8 +1,20 @@
-from flask import render_template, request
+from flask import session, redirect, url_for, render_template, request
 from . import main
 
 
 @main.route('/', methods=['GET'])
 def index():
     """Login form to enter a room."""
-    return render_template('index.html')
+    session['name'] = "testname"
+    session['room'] = "testroom"
+    return redirect(url_for('.chat'))
+
+@main.route('/chat')
+def chat():
+    """Chat room. The user's name and room must be stored in
+    the session."""
+    name = session.get('name', '')
+    room = session.get('room', '')
+    if name == '' or room == '':
+        return redirect(url_for('.index'))
+    return render_template('chat.html', name=name, room=room)
