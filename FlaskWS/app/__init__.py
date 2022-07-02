e5d587240e5f6a3b4504dd83dcc91bed9e0aa0cb
John Brodnax <JBrodnax320@jbrodnax320.local>
2022-07-02 15:16:33 -0500

    added cors

--- FlaskWS/app/__init__.py
+++ FlaskWS/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 socketio = SocketIO()
 
@@ -7,6 +8,7 @@ socketio = SocketIO()
 def create_app(debug=False):
     """Create an application."""
     app = Flask(__name__)
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
 
